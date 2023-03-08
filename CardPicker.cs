53f09868c2b22753ce72eb1f907ef77082cdd13e
Arda Kurt <to.arda.kurt@gmail.com>
2023-03-08 11:59:45 +0000

    Chapter 3 Done

---
+++ CardPicker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace PickRandomCards
+{
+	class CardPicker
+	{
+
+        static Random random = new Random();
+
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+
+            return pickedCards;
+
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            if (value == 1) return "Ace";
+            if (value == 11) return "Jack";
+            if (value == 12) return "Quenn";
+            if (value == 13) return "King";
+            return value.ToString();
+        }
+
+        private static string RandomSuit()
+        {
+
+            int value = random.Next(1, 5);
+
+            if (value == 1) return "Spades";
+            if (value == 2) return "Hearts";
+            if (value == 3) return "Clubs";
+            return "Diamonds";
+
+
+        }
+
+
+
+    }
+
+	
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PickRandomCards;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number of cards to pick: ");
+        string line = Console.ReadLine();
+
+        if (int.TryParse(line, out int numberOfCards))
+        {
+            foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+            {
+                Console.WriteLine(card);
+               
+            }
+        } else
+        {
+            Console.WriteLine("Please type valid number");
+        }
+    }
+}
+
